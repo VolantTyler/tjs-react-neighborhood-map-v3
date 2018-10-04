b5a9929b0b5a1ebbe8b71497adfb84c3b6467a80
VolantTyler <33227593+VolantTyler@users.noreply.github.com>
2018-10-04 13:46:43 -0400

    feat: map markers load

--- src/App.js
+++ src/App.js
@@ -5,19 +5,45 @@ import SquareAPI from "./API/";
 
 class App extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      venues: [],
+      markers: [],
+      center: [],
+      zoom: 12
+    }
+  }
+
   componentDidMount() {
     SquareAPI.search({
       near: "Ridgewood, NJ",
       query: "burgers",
-      limit: 10
+      limit: 20,
+      // radius: 250
     })
-    .then(results => console.log(results));
+    .then(results => {
+      const {venues} = results.response;
+      const {center} = results.response.geocode.feature.geometry;
+      const markers = venues.map(venue => {
+        return {
+          lat: venue.location.lat,
+          lng: venue.location.lng,
+          isOpen: false,
+          isVisible: true
+        };
+      });
+      this.setState({ venues, center, markers });
+      console.log(results);
+    });
   }
 
   render() {
     return (
       <div className="App">
-        <Map />
+        <Map 
+          {...this.state}
+        />
       </div>
     );
   }
