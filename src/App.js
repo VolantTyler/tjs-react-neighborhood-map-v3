23c05a1a699a3f659b26055a4b752ceedf0b48df
VolantTyler <33227593+VolantTyler@users.noreply.github.com>
2018-10-04 16:35:53 -0400

    feat: openning new info window closes any open info windows first

--- src/App.js
+++ src/App.js
@@ -15,7 +15,15 @@ class App extends Component {
     };
   }
 
+  closeAllMarkers = () => {
+    const markers = this.state.markers.map(marker => {
+      marker.isOpen = false;
+      return marker;
+    })
+    this.setState({markers: Object.assign(this.state.markers, markers)});
+  }
   handleMarkerClick = (marker) => {
+    this.closeAllMarkers();
     marker.isOpen = true;
     this.setState({markers: Object.assign(this.state.markers, marker)});
   };
