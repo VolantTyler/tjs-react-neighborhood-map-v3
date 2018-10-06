889994344356d5ee9b15da1c298f2fdd47e2e330
VolantTyler <33227593+VolantTyler@users.noreply.github.com>
2018-10-06 16:38:10 -0400

    feat: add tabindex to list items for focus

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -16,7 +16,7 @@ class ListItem extends Component {
             //     {this.props.venue.name}
             // </li>
             //TODO: can add icon from this.props.categories[0].icon.prefix+"32"+this.props.categories[0].icon.suffix
-            <div className='list-item' onClick={() => this.props.handleListItemClick(this.props)}>
+            <div className='list-item' tabIndex='0' onClick={() => this.props.handleListItemClick(this.props)}>
               {this.props.name}
             </div>
         )
